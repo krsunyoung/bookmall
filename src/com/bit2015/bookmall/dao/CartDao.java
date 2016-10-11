160a40861c6d67ae219e75d9a232694a61879972
sunyoung <bit-user1@bit-user>
2016-10-11 17:52:01 +0900

    1011

--- src/com/bit2015/bookmall/dao/CartDao.java
+++ src/com/bit2015/bookmall/dao/CartDao.java
@@ -9,9 +9,10 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.bit2016.bookmall.vo.BookVo;
+import com.bit2016.bookmall.vo.CartVo;
+import com.bit2016.bookmall.vo.OrdergoVo;
 
-public class BookDao {
+public class CartDao {
 	private Connection getConnection() throws SQLException{ //코드 중복으로 계속 사용해서 만들어줌. 수정일 경우에 다 하나씩 고쳐야 하므로 이것을 만들어서 한번에 수정.
 		Connection conn = null;
 		try {
@@ -23,17 +24,17 @@ public class BookDao {
 		} 
 		return conn;
 	}
-	public boolean update(BookVo vo){
+	public boolean update(CartVo vo){
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		int result= 0;
 		try{
 			conn = getConnection();
-			String sql ="update book set title = ? where no=?";
+			String sql ="update ordergo set address = ? where no=?";
 			pstmt = conn.prepareStatement(sql);
 			
 			
-			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(1, vo.getAddress());
 			pstmt.setLong(2, vo.getNo());
 			
 			result = pstmt.executeUpdate();
@@ -62,7 +63,7 @@ public class BookDao {
 		int result= 0;
 		try{
 			conn = getConnection();
-			String sql ="delete from book where no = ?";
+			String sql ="delete from cart where no = ?";
 			pstmt = conn.prepareStatement(sql);
 			
 			pstmt.setLong(1, no);
@@ -114,8 +115,8 @@ public class BookDao {
 //			}
 //		}
 //	}
-	public List<BookVo> getList(){
-		List<BookVo> list = new ArrayList<BookVo>();
+	public List<CartVo> getList(){
+		List<CartVo> list = new ArrayList<CartVo>();
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
@@ -124,18 +125,18 @@ public class BookDao {
 			conn = getConnection();
 			stmt = conn.createStatement();
 			
-			String sql = "select no, title,price from book ";
+			String sql = "select book_no, customer_no, count from cart ";
 			rs = stmt.executeQuery(sql);
 			
 			while(rs.next()){
-				Long no = rs.getLong(1);
-				String title = rs.getString(2);
-				int price = rs.getInt(3);
+				int book_no = rs.getInt(1);
+				int customer_no = rs.getInt(2);
+				int count = rs.getInt(3);
 				
-				BookVo vo = new BookVo();
-				vo.setNo(no);
-				vo.setTitle(title);
-				vo.setPrice(price);
+				CartVo vo = new CartVo();
+				vo.setBook_no(book_no);
+				vo.setCustomer_no(customer_no);
+				vo.setCount(count);
 				
 				list.add(vo);
 			}
@@ -163,7 +164,7 @@ public class BookDao {
 //	public void insert(String name){
 //		
 //	}
-	public void insert(BookVo vo){
+	public void insert(OrdergoVo vo){
 	
 			
 			Connection conn =null;
@@ -173,13 +174,13 @@ public class BookDao {
 				 conn = getConnection();
 
 				 //3.statement 생성
-				 String sql = "insert into book Values(book_seq.nextval, ?,?,?)";
+				 String sql = 
+				"insert into cart Values(?,?,?)";
 				 pstmt = conn.prepareStatement(sql);
 				 
 				 //4.바인딩
-				 pstmt.setLong(1, vo.getCate_no());
-				 pstmt.setString(2, vo.getTitle());
-				 pstmt.setInt(3, vo.getPrice());
+				 pstmt.setInt(1, vo.getPrice());
+				 pstmt.setString(2, vo.getAddress());
 				 
 				 int count = pstmt.executeUpdate();
 				 System.out.println( count );
